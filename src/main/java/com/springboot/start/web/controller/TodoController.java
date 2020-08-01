f5252ccac6f87a4e6b74dbe34daaeb37d8cf52ed
Ankit <ankitvartak@gmail.com>
2020-08-01 18:38:08 -0400

    @SessionAttributes for name

--- src/main/java/com/springboot/start/web/controller/TodoController.java
+++ src/main/java/com/springboot/start/web/controller/TodoController.java
@@ -6,16 +6,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("name")
 public class TodoController {
     @Autowired
     TodoService todoService;
 
     @RequestMapping(value="/list-todos", method = RequestMethod.GET)
     public String showTodos(ModelMap model) {
-        model.put("todos", todoService.retrieveTodos("test"));
+        String username = (String) model.get("name");
+        model.put("todos", todoService.retrieveTodos(username));
         return "/list-todos";
     }
 
--- src/main/webapp/WEB-INF/jsp/list-todos.jsp
+++ src/main/webapp/WEB-INF/jsp/list-todos.jsp
@@ -1,7 +1,7 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>User Todo List</title>
+    <title>${name}'s Todo List</title>
 </head>
 <body>
  List to be shown
